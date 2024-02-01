6526e6a0878dffaff064c820a82f9af1a2edbc65
Mayur <mayurjreddy@gmail.com>
2024-02-01 14:50:46 -0500

    Unequipped crouch and Tick Rate

--- Source/ParkourShooter/ParkourShooterCharacter.cpp
+++ Source/ParkourShooter/ParkourShooterCharacter.cpp
@@ -73,6 +73,9 @@ AParkourShooterCharacter::AParkourShooterCharacter(const FObjectInitializer& Obj
 	Combat = CreateDefaultSubobject<UCombatComponent>(TEXT("CombatComponent"));
 	Combat->SetIsReplicated(true);
 
+	NetUpdateFrequency = 66.f;
+	MinNetUpdateFrequency = 33.f;
+
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
 }
